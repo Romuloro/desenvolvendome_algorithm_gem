959754f3194d277e5d94561e2d53a9b2c226e513
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-07-19 10:47:34 -0300

    feat: create find missing number function

---
+++ Algorith_nvl3/find_missing_number.rb
@@ -0,0 +1,9 @@
+def missingNumber(array)
+    n = array.length
+    sum_array = array.sum
+    total = (n + 1)*(n + 2)/2
+    return total - sum_array
+end
+
+arr = [1, 2, 3, 5]
+p missingNumber(arr)
