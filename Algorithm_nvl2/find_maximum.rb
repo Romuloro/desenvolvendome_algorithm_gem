bebc37c137f7723e104115fb5c1b052763c983a1
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-07-27 11:16:06 -0300

    feat: create find maximum function

---
+++ Algorithm_nvl2/find_maximum.rb
@@ -0,0 +1,36 @@
+def findMaximum(array1, array2, array3)
+    len1 = array1.length - 1
+    len2 = array2.length - 1
+    len3 = array3.length - 1
+    sum1, sum2, sum3 = array1.sum, array2.sum, array3.sum
+    element1, element2, element3 = 0, 0, 0
+
+    while(true) do
+        if (element1 == len1 or element2 == len2 or element3 == len3)
+            return false    
+        end
+
+        if (sum1 == sum2 and sum2 == sum3)
+            return sum1
+        end
+
+        if (sum1 >= sum2 and sum1 >= sum3)
+            sum1 -= array1[element1]
+            element1 += 1
+        elsif (sum2 >= sum1 and sum2 >= sum3)
+            sum2 -= array2[element2]
+            element2 += 1
+        elsif (sum3 >= sum2 and sum3 >= sum1)
+            sum3 -= array3[element3]
+            element3 += 1
+        end
+    end
+end
+
+stack1 = [ 3, 2, 1, 1, 1 ]
+stack2 = [ 4, 3, 2 ]
+stack3 = [ 1, 1, 4, 1 ]
+
+find = findMaximum(stack1, stack2, stack3)
+
+p find
