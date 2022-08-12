b10d8bc8c00dcdac9101637e31b4e6744f943119
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-08-12 11:56:49 -0300

    test(add secund test):

--- spec/lib/job_sequencing_problem_spec.rb
+++ spec/lib/job_sequencing_problem_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe "Greedy::JobSequencingProblem" do
     sum_result = Greedy::JobSequencingProblem.new.run(arr, k)
     expect(sum_result).to eq(["c","a","e"])
   end
+  it "Job Sequencing Problem return c a e " do
+    arr = [['a', 2, 20], ['b', 1, 10], ['c', 1, 40], ['d', 1, 30]]
+    k = 2
+
+    sum_result = Greedy::JobSequencingProblem.new.run(arr, k)
+    expect(sum_result).to eq(["c","a"])
+  end
 end
